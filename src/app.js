c93b21923f61c607a622a763a6af888de128f626
Shivam Gupta <shivam.gupta1@hp.com>
2024-11-15 18:05:19 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 const express= require('express');
 const app = express();
-const {adminAuth} = require('./middlewares/auth');
+//const {adminAuth} = require('./middlewares/auth');
 
-app.use("/admin",adminAuth);
+// app.use("/admin",adminAuth);
 
-app.get("/admin/getAllData",(req,res)=> {
-    res.send('All data sent!');
-});
-app.get("/admin/deleteUser",(req,res)=> {
-    res.send('Deleted a user!');
-});
+// app.get("/admin/getAllData",(req,res)=> {
+//     res.send('All data sent!');
+// });
+// app.get("/admin/deleteUser",(req,res)=> {
+//     res.send('Deleted a user!');
+// });
+    app.get("/getUserData",(req,res)=>{
+            throw new Error("gwqdqaeerg");  
+        res.send('User data sent!');
+
+    });
+    app.use("/",(err,req,res,next)=>{
+        if(err){
+            res.status(500).send("something went wrong");
+        }
+    });
 
 app.listen(3000, () => {   
     console.log('Server is running on port 3000');
