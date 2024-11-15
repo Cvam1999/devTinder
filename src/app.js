cadfab7750448f31ae7224f180e05611af2d1827
Shivam Gupta <shivam.gupta1@hp.com>
2024-11-15 17:35:22 +0530

    Writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express= require('express');
 const app = express();
+const {adminAuth} = require('./middlewares/auth');
 
-app.use((req,res)=> {
-    res.send('Hello from the sever!');
+app.use("/admin",adminAuth);
+
+app.get("/admin/getAllData",(req,res)=> {
+    res.send('All data sent!');
 });
+app.get("/admin/deleteUser",(req,res)=> {
+    res.send('Deleted a user!');
+});
+
 app.listen(3000, () => {   
     console.log('Server is running on port 3000');
 }); 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,11 @@
+  const adminAuth=(req,res,next)=>{
+    console.log('admin auth is getting checked');
+    const token= "xyz";
+    const isAdminAuthorized= token === "xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("unathourized request");
+    }else{
+        next();
+    }
+}
+module.exports={adminAuth};
