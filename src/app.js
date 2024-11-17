6ab18d39802cb326cb640b758ddc1c8defe1723c
Shivam Gupta <shivam.gupta1@hp.com>
2024-11-17 13:41:20 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express= require('express');
 const app = express();
+const connectDB = require('./config/database');
+const e = require('express');
+const User = require('./models/user');
 //const {adminAuth} = require('./middlewares/auth');
 
 // app.use("/admin",adminAuth);
@@ -10,17 +13,41 @@ const app = express();
 // app.get("/admin/deleteUser",(req,res)=> {
 //     res.send('Deleted a user!');
 // });
-    app.get("/getUserData",(req,res)=>{
-            throw new Error("gwqdqaeerg");  
-        res.send('User data sent!');
 
-    });
-    app.use("/",(err,req,res,next)=>{
-        if(err){
-            res.status(500).send("something went wrong");
+    // app.get("/getUserData",(req,res)=>{
+    //         throw new Error("gwqdqaeerg");  
+    //     res.send('User data sent!');
+
+    // });
+    // app.use("/",(err,req,res,next)=>{
+    //     if(err){
+    //         res.status(500).send("something went wrong");
+    //     }
+    // });
+
+    app.post("/signup", async(req,res)=>{
+        const userObj = {
+            firstName: 'Suraj',
+            lastName: 'Joshi',
+            emailId: 'surajjoshi@gmail,com',
+            password: 'suraj@123',
+            age: 25,
+            gender:'Male'
         }
+        const user = new User(userObj);
+        try{
+            await user.save();
+            res.send('User created');
+        }catch(err){
+            res.status(400).send('error while saving user:' + err.message);
+    });
+    
+    connectDB().then(() => {
+        console.log("Database connected");
+        app.listen(3000, () => {   
+            console.log('Server is running on port 3000');
+        }); 
+    }).catch((err) => {
+        console.error("Error connecting database", err);
     });
 
-app.listen(3000, () => {   
-    console.log('Server is running on port 3000');
-}); 
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://shivamgupta27101999:kSJtgEnzGzXS3VD2@namastenodejs.4ycpg.mongodb.net/devTinder"
+);
+};
+module.exports = connectDB;
+
+
