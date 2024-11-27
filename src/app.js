a99eeeb6a790d81e7c29c295337dcf2eb7edef30
Shivam Gupta <shivam.gupta1@hp.com>
2024-11-27 23:32:42 +0530

    pagination

--- src/app.js
+++ src/app.js
@@ -23,64 +23,64 @@ const { userAuth} = require('./middlewares/auth');
   app.use('/', userRouter);
    
 
-    app.get("/user", async(req,res)=>{
-         try{
-             const users = await User.find({emailId: req.body.emailId});
-             if(users.length === 0){
-                    return res.status(404).send('User not found');
-             }
-             else{
-                    return res.send(users);
-             }
-         }catch(err){
-             res.status(400).send('error while fetching user:' + err.message);
-         }
-    });
+    // app.get("/user", async(req,res)=>{
+    //      try{
+    //          const users = await User.find({emailId: req.body.emailId});
+    //          if(users.length === 0){
+    //                 return res.status(404).send('User not found');
+    //          }
+    //          else{
+    //                 return res.send(users);
+    //          }
+    //      }catch(err){
+    //          res.status(400).send('error while fetching user:' + err.message);
+    //      }
+    // });
 
-    app.get("/feed", async(req,res)=>{
-        try{
-            const users = await User.find({});
-            res.send(users);
-        }catch(err){
-            res.status(400).send('error while fetching users:' + err.message);
-        }
-    });
+    // app.get("/feed", async(req,res)=>{
+    //     try{
+    //         const users = await User.find({});
+    //         res.send(users);
+    //     }catch(err){
+    //         res.status(400).send('error while fetching users:' + err.message);
+    //     }
+    // });
   
-    app.delete("/user", async(req,res)=>{
-        const userId = req.body.userId;
-        try{
-            const users = await User.findByIdAndDelete(userId);
-            res.send('User deleted successfully');
-        }catch(err){    
-            res.status(400).send('error while deleting user:' + err.message);
-        }
-    });
-    app.patch("/user/:userId", async(req,res)=>{
-        const userId = req.params?.userId;
-        const data = req.body;
+    // app.delete("/user", async(req,res)=>{
+    //     const userId = req.body.userId;
+    //     try{
+    //         const users = await User.findByIdAndDelete(userId);
+    //         res.send('User deleted successfully');
+    //     }catch(err){    
+    //         res.status(400).send('error while deleting user:' + err.message);
+    //     }
+    // });
+    // app.patch("/user/:userId", async(req,res)=>{
+    //     const userId = req.params?.userId;
+    //     const data = req.body;
 
         
-        try{
-            const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
-        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+    //     try{
+    //         const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    //     const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
 
-        if(!isUpdateAllowed){
-            throw new Error("update not allowed");
-        }
-        if(data?.skills.length> 10){
-            throw new Error("skills array length should be less than 10");
-        }
+    //     if(!isUpdateAllowed){
+    //         throw new Error("update not allowed");
+    //     }
+    //     if(data?.skills.length> 10){
+    //         throw new Error("skills array length should be less than 10");
+    //     }
 
-            const users = await User.findByIdAndUpdate(userId, data, {
-                returnDocument: 'after',
-                runValidators: true
-            }); 
-            console.log(users);         
-            res.send('User updated successfully');
-        }catch(err){
-            res.status(400).send('error while updating user: ' + err.message);
-        }
-    });
+    //         const users = await User.findByIdAndUpdate(userId, data, {
+    //             returnDocument: 'after',
+    //             runValidators: true
+    //         }); 
+    //         console.log(users);         
+    //         res.send('User updated successfully');
+    //     }catch(err){
+    //         res.status(400).send('error while updating user: ' + err.message);
+    //     }
+    // });
     
     connectDB().then(() => {
         console.log("Database connected");
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const userRouter = express.Router();
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const USER_SAFE_DATA="firstName lastName photoUrl about age gender skills";
 //get all the pending connection request for the loggedin user
@@ -48,6 +49,52 @@ userRouter.get("/user/connections",userAuth, async(req,res)=>{
         });
     }catch(err){
         res.status(400).send('error while fetching connections:' + err.message);
+    }
+});
+userRouter.get("/feed",userAuth, async(req,res)=>{
+    try{
+
+       const loggedInUser = req.user;
+       const page = parseInt(req.query.page) || 1;
+       let limit = parseInt(req.query.limit) || 10;
+       limit = limit > 50 ? 50 : limit;
+       const skip = (page - 1) * limit;
+
+       const connectionRequests = await ConnectionRequest.find({
+           $or: [
+               {
+                   toUserId: loggedInUser._id,
+                  
+               },
+               {
+                   fromUserId: loggedInUser._id,
+                  
+               }
+           ]
+           
+       }).select("fromUserId toUserId");
+       const hideUsersFromFeed = new set();
+         connectionRequests.forEach((row)=>{
+              hideUsersFromFeed.add(row.fromUserId.toString());
+              hideUsersFromFeed.add(row.toUserId.toString());
+         });
+         const users = await User.find({
+             $and: [
+                 {
+                     _id: {
+                         $nin: [...hideUsersFromFeed]
+                     }
+                 },
+                 {
+                     _id: {
+                         $ne: loggedInUser._id
+                     }
+                 }
+             ]
+         }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+       res.send(users);
+    }catch(err){
+        res.status(400).send('error while fetching feed:' + err.message);
     }
 });
 module.exports = userRouter;
